75c3406acb29fe3d11fb72f1c4af4a52fa8c59b8
armentanoc <armentanocarolina@gmail.com>
2024-01-06 13:19:39 -0300

    feat: implements currency conversion.

---
+++ ExerciseListOOP.Domain/Model/CurrencyConverter/StringToDoubleConverter.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+public class StringToDoubleConverter : JsonConverter<double>
+{
+    public override double Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (reader.TokenType == JsonTokenType.Number)
+        {
+            return reader.GetDouble();
+        }
+
+        if (reader.TokenType == JsonTokenType.String && double.TryParse(reader.GetString(), out double result))
+        {
+            return result;
+        }
+
+        throw new JsonException($"Não foi possível converter {reader.TokenType} para {typeToConvert}");
+    }
+
+    public override void Write(Utf8JsonWriter writer, double value, JsonSerializerOptions options)
+    {
+        writer.WriteNumberValue(value);
+    }
+}
