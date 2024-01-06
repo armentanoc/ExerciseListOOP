35cfdf4143d3d733d6e88f2afcca613182809e05
armentanoc <armentanocarolina@gmail.com>
2024-01-06 13:19:31 -0300

    feat: implements currency conversion.

---
+++ ExerciseListOOP.Domain/Model/CurrencyConverter/ExchangeRateApiClient.cs
@@ -0,0 +1,100 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace ExerciseListOOP.ConsoleInteraction.Components
+{
+    public class ExchangeRateApiClient
+    {
+        private readonly string _apiBaseUrl = "https://economia.awesomeapi.com.br/json/";
+        private readonly HttpClient _httpClient;
+
+        public ExchangeRateApiClient()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<double> GetExchangeRate()
+        {
+            string key = "USD";
+            try
+            {
+                string apiUrl = $"{_apiBaseUrl}{key}";
+                HttpResponseMessage response = await _httpClient.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    return ParseExchangeRate(content);
+                }
+                else
+                {
+                    HandleErrorResponse(response);
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                HandleHttpRequestException(ex);
+            }
+            catch (Exception ex)
+            {
+                HandleGeneralException(ex);
+            }
+
+            // default value only
+            return -1;
+        }
+
+        private double ParseExchangeRate(string content)
+        {
+            using (JsonDocument document = JsonDocument.Parse(content))
+            {
+                JsonElement root = document.RootElement;
+                if (root.ValueKind == JsonValueKind.Array && root.GetArrayLength() > 0)
+                {
+                    JsonElement highElement = root[0].GetProperty("high");
+
+                    if (highElement.ValueKind == JsonValueKind.String)
+                    {
+                        if (double.TryParse(highElement.GetString(), out double highValue))
+                        {
+                            Console.Clear();
+                            Message.WriteTitle(Title.CurrencyConverter(), "Green");
+                            Console.WriteLine($"\nConversão atual BRL-USD: 1-{highValue}");
+                            return highValue;
+                        }
+                        else
+                        {
+                            Message.Error("Erro ao converter o valor da propriedade 'high' para um número válido.");
+                        }
+                    }
+                    else
+                    {
+                        Message.Error("A propriedade 'high' não é uma string.");
+                    }
+                }
+                else
+                {
+                    Message.Error("A resposta JSON não é um array ou está vazia.");
+                }
+            }
+
+            return -1; // default value
+        }
+
+        private void HandleErrorResponse(HttpResponseMessage response)
+        {
+            Message.Error($"Erro na taxa de conversão. Status code: {response.StatusCode}");
+        }
+
+        private void HandleHttpRequestException(HttpRequestException ex)
+        {
+            Message.Error($"HTTP Request Error: {ex.Message}");
+        }
+
+        private void HandleGeneralException(Exception ex)
+        {
+            Message.Error($"Erro na taxa de conversão: {ex.Message}");
+        }
+    }
+}
---
+++ ExerciseListOOP.Domain/Service/CurrencyConverter.cs
@@ -0,0 +1,99 @@
+﻿using ExerciseListOOP.ConsoleInteraction;
+using ExerciseListOOP.ConsoleInteraction.Components;
+
+namespace ExerciseListOOP.Domain.Service
+{
+    internal class CurrencyConverter : IMenuConvertible
+    {
+        private readonly Menu _mainMenu;
+        private readonly string MenuTitle = Title.CurrencyConverter();
+        private readonly string TitleColor = "Green";
+        private readonly string[] _mainMenuOptions = { "Converter Real para Dólar", "Converter Dólar para Real", "Sair" };
+
+        private readonly ExchangeRateApiClient _exchangeRateApiClient;
+
+        public CurrencyConverter()
+        {
+            _mainMenu = new Menu(_mainMenuOptions);
+            _exchangeRateApiClient = new ExchangeRateApiClient();
+        }
+
+        public int Display(string title, string color)
+        {
+            return _mainMenu.DisplayMenu(title, color);
+        }
+
+        public void Analyze(int selectedOption)
+        {
+            switch (selectedOption)
+            {
+                case 0:
+                    ConvertCurrency(true); 
+                    break;
+                case 1:
+                    ConvertCurrency(false);
+                    break;
+                case 2:
+                    HandleExitOption();
+                    break;
+                default:
+                    Message.Error("Opção inválida. Por favor, escolha 1, 2 ou 3.");
+                    Message.PressAnyKeyToContinue();
+                    break;
+            }
+        }
+
+        private void ConvertCurrency(bool reverseConversion)
+        {
+            string fromCurrency = reverseConversion ? "BRL" : "USD";
+            string toCurrency = reverseConversion ? "USD" : "BRL";
+
+            try
+            {
+                double exchangeRate = _exchangeRateApiClient.GetExchangeRate().Result;
+
+                if (exchangeRate > 0)
+                {
+                    Console.Write($"\nDigite o valor a ser convertido ({fromCurrency}): ");
+                    double.TryParse(Console.ReadLine(), out double amountToConvert);
+
+                    double convertedAmount = reverseConversion ? amountToConvert / exchangeRate : amountToConvert * exchangeRate;
+
+                    Message.LogAndConsoleWrite($"Resultado da conversão de {amountToConvert}{fromCurrency} para {toCurrency}: \n{convertedAmount:F2} {toCurrency}");
+                }
+                else
+                {
+                    Message.Error("Não foi possível obter a taxa de câmbio. Verifique sua conexão com a internet e tente novamente.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Message.Error($"Erro durante a conversão de moeda: {ex.Message}");
+            }
+
+            Message.PressAnyKeyToContinue();
+        }
+
+
+        private void HandleExitOption()
+        {
+            string environmentExit = Title.InnerProgramExit();
+            Message.WriteTitle(environmentExit);
+        }
+
+        public int GetMainMenuLength()
+        {
+            return _mainMenuOptions.Length;
+        }
+
+        public string GetTitle()
+        {
+            return MenuTitle;
+        }
+
+        public string GetTitleColor()
+        {
+            return TitleColor;
+        }
+    }
+}
