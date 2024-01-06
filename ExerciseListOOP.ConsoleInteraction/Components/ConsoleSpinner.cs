5342c46b88a68ccaddf32570cfa3c12abba6fe50
armentanoc <armentanocarolina@gmail.com>
2024-01-06 09:16:28 -0300

    feat: adds console spinner class.

--- ExerciseListOOP.ConsoleInteraction/Components/ConsoleSpinner.cs
+++ ExerciseListOOP.ConsoleInteraction/Components/ConsoleSpinner.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace ExerciseListOOP.ConsoleInteraction.Components
 {
-    internal class ConsoleSpinner
+    public class ConsoleSpinner
     {
+        int counter;
+        public ConsoleSpinner()
+        {
+            counter = 0;
+        }
+        public void Turn()
+        {
+            counter++;
+            switch (counter % 4)
+            {
+                case 0: Console.Write("/"); break;
+                case 1: Console.Write("-"); break;
+                case 2: Console.Write("\\"); break;
+                case 3: Console.Write("|"); break;
+            }
+            Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+        }
     }
 }
